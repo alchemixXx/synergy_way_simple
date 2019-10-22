094c7f83c2e3808f8ded89cc2323659a6b992336
Timon384 <pumba324@gmail.com>
2019-10-22 17:46:43 +0300

    create 2 clear pages install create-bootstrap

--- synergy_way_simple/src/App.js
+++ synergy_way_simple/src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Users from "./components/List_of_Users";
+import Grous from "./components/List_of_Groups";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+               <Users/>
+               <Grous/>
+
+            </div>
+        );
+
+    }
 }
 
 export default App;
+
